115855e10ae2b0ab0e6325dfa0f6fcf645a938d0
Sergii Ozinkovskyi <s.ozinkovskyi@gmail.com>
2019-11-26 00:10:21 +0200

    removed properties height: 100% in wrapper, add main_info-list

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// let secInMin = 60;
+// let minInHour = 60;
+// let hourInDay = 24;
+// let daysInYear = 365;
+
+// let tys = secInMin * minInHour * hourInDay * daysInYear * 3;
+
+// console.log("Three years have: " + tys + " seconds");
+
+
+// let daysInThreeYears = daysInYear * 3;
+
+// console.log("Three years have: " + daysInThreeYears + " days");
+
+let first = prompt("give me a first number: ");
+let second = prompt("give me a second number: ");
+let summ = parseInt(first) + parseInt(second);
+alert(summ);
+
